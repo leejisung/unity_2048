a0fe9a332485fa9e355bbd7c777eaee0b17296f4
leejisung <starseed13@hanmail.net>
2021-01-12 15:50:49 +0900

    스파게티 정리

--- GameManager.cs
+++ GameManager.cs
@@ -13,7 +13,84 @@ public class GameManager : MonoBehaviour
     // GameManager 에서 사용 하는 데이터
     public bool isPause = false;
     public int[,] board = new int[4, 4] { { 2, 2, 2, 2 }, { 4, 2, 2, 2 }, { 8, 8, 16, 0 }, { 16, 0, 4, 4 } };
-    
+    public void row_reverse()
+    {
+        int[,] change = new int[4, 4];
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                change[i, j] = board[i, 3 - j];
+            }
+        }
+        board = change;
+    }
+    public void loc_reverse()
+    {
+        int[,] change = new int[4, 4];
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                change[i, j] = board[j, i];
+            }
+        }
+        board = change;
+    }
+    public void left_key()
+    {
+        int[,] change = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+        for (int i = 0; i < 4; i++)
+        {
+            int xx = 0;
+            for (int j = 0; j < 4; j++)
+            {
+                if (board[i, j] != 0)
+                {
+                    change[i, xx] = board[i, j];
+                    xx += 1;
+                }
+            }
+        }
+        board = change;
+        for (int i = 0; i < 4; i++)
+        {
+            if (board[i, 0] == board[i, 1] &&
+                board[i, 2] == board[i, 3])
+            {
+                int num = board[i, 0] * 2;
+                int num2 = board[i, 2] * 2;
+                board[i, 0] = num;
+                board[i, 1] = num2;
+                board[i, 2] = 0;
+                board[i, 3] = 0;
+            }
+            else if (board[i, 0] == board[i, 1])
+            {
+                int num = board[i, 0] * 2;
+                int num2 = board[i, 2];
+                int num3 = board[i, 3];
+                board[i, 0] = num;
+                board[i, 1] = num2;
+                board[i, 2] = num3;
+                board[i, 3] = 0;
+            }
+            else if (board[i, 1] == board[i, 2])
+            {
+                int num2 = board[i, 1] * 2;
+                int num3 = board[i, 3];
+                board[i, 1] = num2;
+                board[i, 2] = num3;
+                board[i, 3] = 0;
+            }
+            else if (board[i, 2] == board[i, 3])
+            {
+                int num3 = board[i, 3] * 2;
+                board[i, 2] = num3;
+                board[i, 3] = 0;
+            }
+        }
+    }
     public static GameManager Instance
     {
         get
--- input.cs
+++ input.cs
@@ -4,109 +4,32 @@ using UnityEngine;
 
 public class input : MonoBehaviour
 {
-    void row_reverse()
-    {
-        int[,] change = new int[4, 4];
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                change[i, j] = GameManager.Instance.board[i, 3 - j];
-            }
-        }
-        GameManager.Instance.board = change;
-    }
-    void loc_reverse()
-    {
-        int[,] change = new int[4, 4];
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                change[i, j] = GameManager.Instance.board[j,i];
-            }
-        }
-        GameManager.Instance.board = change;
-    }
-    void left_key()
-    {
-        int[,] change = new int[4, 4] { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
-        for (int i = 0; i < 4; i++)
-        {
-            int xx = 0;
-            for (int j = 0; j < 4; j++)
-            {
-                if (GameManager.Instance.board[i,j]!=0)
-                {
-                    change[i, xx] = GameManager.Instance.board[i, j];
-                    xx += 1;
-                }
-            }
-        }
-        GameManager.Instance.board = change;
-        for (int i = 0; i < 4; i++)
-        {
-            if (GameManager.Instance.board[i, 0] == GameManager.Instance.board[i, 1] &&
-                GameManager.Instance.board[i, 2] == GameManager.Instance.board[i, 3])
-            {
-                int num = GameManager.Instance.board[i, 0] * 2;
-                int num2 = GameManager.Instance.board[i, 2] * 2;
-                GameManager.Instance.board[i, 0] = num;
-                GameManager.Instance.board[i, 1] = num2;
-                GameManager.Instance.board[i, 2] = 0;
-                GameManager.Instance.board[i, 3] = 0;
-            }
-            else if (GameManager.Instance.board[i, 0] == GameManager.Instance.board[i, 1])
-            {
-                int num = GameManager.Instance.board[i, 0] * 2;
-                int num2 = GameManager.Instance.board[i, 2];
-                int num3 = GameManager.Instance.board[i, 3];
-                GameManager.Instance.board[i, 0] = num;
-                GameManager.Instance.board[i, 1] = num2;
-                GameManager.Instance.board[i, 2] = num3;
-                GameManager.Instance.board[i, 3] = 0;
-            }
-            else if (GameManager.Instance.board[i, 1] == GameManager.Instance.board[i, 2])
-            {
-                int num2 = GameManager.Instance.board[i, 1] * 2;
-                int num3 = GameManager.Instance.board[i, 3];
-                GameManager.Instance.board[i, 1] = num2;
-                GameManager.Instance.board[i, 2] = num3;
-                GameManager.Instance.board[i, 3] = 0;
-            }
-            else if (GameManager.Instance.board[i, 2] == GameManager.Instance.board[i, 3])
-            {
-                int num3 = GameManager.Instance.board[i, 3] * 2;
-                GameManager.Instance.board[i, 2] = num3;
-                GameManager.Instance.board[i, 3] = 0;
-            }
-        }
-    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            left_key();
+            GameManager.Instance.left_key();
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            row_reverse();
-            left_key();
-            row_reverse();
+            GameManager.Instance.row_reverse();
+            GameManager.Instance.left_key();
+            GameManager.Instance.row_reverse();
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            loc_reverse();
-            left_key();
-            loc_reverse();
+            GameManager.Instance.loc_reverse();
+            GameManager.Instance.left_key();
+            GameManager.Instance.loc_reverse();
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            loc_reverse();
-            row_reverse();
-            left_key();
-            row_reverse();
-            loc_reverse();
+            GameManager.Instance.loc_reverse();
+            GameManager.Instance.row_reverse();
+            GameManager.Instance.left_key();
+            GameManager.Instance.row_reverse();
+            GameManager.Instance.loc_reverse();
         }
     }
 }
